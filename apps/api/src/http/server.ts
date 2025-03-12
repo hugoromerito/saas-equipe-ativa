70d9392850a7124f8a79356ae10590caa1ec73ef
Hugo de Souza de Queiroz <hugoxvida@gmail.com>
2025-03-12 17:08:56 -0300

    authenticate with password

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,15 +1,16 @@
 import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUI from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
   jsonSchemaTransform,
-  // jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 
+import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { createAccount } from './routes/auth/create-account'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -33,6 +34,10 @@ app.register(fastifySwaggerUI, {
   routePrefix: '/documentation',
 })
 
+app.register(fastifyJwt, {
+  secret: 'my-jwt-secret',
+})
+
 app.register(fastifyCors, {
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -42,6 +47,7 @@ app.register(fastifyCors, {
 })
 
 app.register(createAccount)
+app.register(authenticateWithPassword)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
