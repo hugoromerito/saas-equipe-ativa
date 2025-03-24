ce254da3958e1524a20632161c64e7ae3e40ff42
Hugo de Souza de Queiroz <hugoxvida@gmail.com>
2025-03-24 18:07:00 -0300

    altera o height do login

--- apps/web/src/app/auth/layout.tsx
+++ apps/web/src/app/auth/layout.tsx
@@ -12,7 +12,7 @@ export default async function AuthLayout({
   }
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center px-4">
+    <div className="flex max-h-screen flex-col items-center justify-center px-4">
       <div className="w-full max-w-xs">{children}</div>
     </div>
   )
