c1d12317279b5e7b600ecefcada6d7c7b2401c66
Hugo de Souza de Queiroz <hugoxvida@gmail.com>
2025-04-02 01:05:50 -0300

    atualiza o botao do zap

--- apps/web/src/components/whatsapp-button.tsx
+++ apps/web/src/components/whatsapp-button.tsx
@@ -3,10 +3,10 @@ import { Phone } from 'lucide-react'
 export default function WhatsappButton() {
   return (
     <a
-      href="https://wa.me/5521990550811" // Substitua pelo seu número
+      href="https://wa.me/5521983327985" // Substitua pelo seu número
       target="_blank"
       rel="noopener noreferrer"
-      className="group fixed right-4 bottom-4 z-50 flex h-14 w-14 items-center overflow-hidden rounded-full bg-green-500 text-white shadow-lg transition-all duration-300 ease-in-out hover:w-40"
+      className="group fixed right-8 bottom-8 z-50 flex h-14 w-14 items-center overflow-hidden rounded-full bg-green-500 text-white shadow-lg transition-all duration-300 ease-in-out hover:w-40"
     >
       <div className="flex h-14 w-14 items-center justify-center">
         <Phone size={24} />
